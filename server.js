6ba697a97ee1600d80b771cac7866b00cf939d8b
Tarun <tjasw549@mtroyal.ca>
2025-12-06 21:29:18 -0700

    added a root message

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ const express = require('express');
 
 const app = express();
 
+// root page
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to the Art Gallery API",
+        endpoints: [
+            "/api/artists",
+            "/api/artists/:country",
+            "/api/galleries",
+            "/api/galleries/:country",
+            "/api/paintings",
+            "/api/painting/:id",
+            "/api/painting/gallery/:id",
+            "/api/painting/artist/:id",
+            "/api/painting/year/:min/:max",
+            "/api/painting/title/:text",
+            "/api/painting/color/:name"
+        ]
+    });
+});
+
 // artists
 const artists = require("./scripts/artist-router.js");
 artists.handleAllArtists(app);
